af42a67bb47372f09ca5f12c5becbedff29a261a
Stuart Estevez <stuart264.se@gmail.com>
2020-07-09 15:25:34 -0400

    klk2

--- Votar/Votar/Program.cs
+++ Votar/Votar/Program.cs
@@ -9,7 +9,6 @@ namespace Votar
             Console.WriteLine("Este es un sistema de votación aleatorio. \n" + "\nLos candidatos son: \n" + "\n1.El Penco (Presidente). Partido: Partido Ladrones Dominicanos (PLD). Año: 1233. \n" + "2.El Jojoto Blanco (Presidente). Partido: Partido Rico de Mami (PRM). Año: 2039. \n" + "3.El Lidel (Presidente). Fucking Para (FP). Año: 2078. \n" + "4.El Moreno (Presidente). Abusadores del País (ALPAÍS). Año: 2000. \n");
             Random rng = new Random();
             int[] nc = {0, 0, 0, 0};
-            int[] orden = {1, 2, 3, 4};
             double total, op1, op2, op3, op4;
 
             for (int i = 1; i < 1001; i++)
@@ -33,24 +32,6 @@ namespace Votar
                 }
             }
             Console.WriteLine("Aqui están los votos: \n" + "\nEl Penco tiene: " + nc[0] + " votos." + "\n" + "El Jojoto Blanco tiene: " + nc[1] + " votos." + "\n" + "El Lidel tiene: " + nc[2] + " votos." + "\n" + "El Moreno tiene: " + nc[3] + " votos." + "\n");
-            Console.WriteLine("Orden por total de votos: " + "\n");
-            for (int i = 0; i < nc.Length; i++)
-            {
-                for (int j = i + 1; j < nc.Length; j++)
-                {
-                    int aux;
-                    if(nc[i] > nc[j])
-                    {
-                        aux = nc[i];
-                        nc[i] = nc[j];
-                        nc[j] = aux;
-                    }
-                }
-            }
-            for (int i = 3; i >= 0; i--)
-            {
-                Console.WriteLine(orden[i] + "." + nc[i]);
-            }
 
             total = nc[0] + nc[1] + nc[2] + nc[3];
             op1 = (nc[0] / total) * 100;
@@ -99,6 +80,25 @@ namespace Votar
             if (nc[1] == nc[3])
             {
                 Console.WriteLine("HUBO UN EMPATE ENTRE EL JOJOTO BLANCO Y EL MORENO");
+            }
+            Console.WriteLine("\nOrden por total de votos: " + "\n");
+            for (int i = 0; i < nc.Length; i++)
+            {
+                for (int j = i + 1; j < nc.Length; j++)
+                {
+                    int aux;
+                    if (nc[i] > nc[j])
+                    {
+                        aux = nc[i];
+                        nc[i] = nc[j];
+                        nc[j] = aux;
+                    }
+                }
+
+            }
+            for (int i = 3; i >= 0; i--)
+            {
+                Console.WriteLine(nc[i]);
             }
             Console.ReadKey();
 
